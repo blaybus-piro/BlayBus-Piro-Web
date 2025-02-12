4647bce8a5e7d98f7ec193f151237c47ea564a30
Jihyeon Lee <ljh130334@naver.com>
2025-02-12 15:25:45 +0900

    feat(#3) :: Landing Page 구현 완료 및 라우터 설정

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,28 @@
+import "../styles/landing.styles.css";
+
+export default function LandingPage() {
+  return (
+    <div className="container">
+      <header className="header">
+        <img src="/icons/logo.svg" alt="logo" />
+      </header>
+
+      <div className="hero-container">
+        <section className="hero-section-1">
+          <h2 className="hero-text-1">당신을 위한</h2>
+        </section>
+
+        <section className="hero-section-2">
+          <h2 className="hero-text-2">헤어스타일</h2>
+        </section>
+      </div>
+
+      <div className="button-container">
+        <button className="google-login-btn">
+          <img src="/icons/google-icon.svg" alt="Google" />
+          구글로 시작하기
+        </button>
+      </div>
+    </div>
+  );
+}
