cc1e7b459006576e8d4927e05b9d85735aac28d8
fr0gydev <ljh130334@naver.com>
2025-02-15 16:44:26 +0900

    fix :: 라우터 수정(최종 전에 한 번 더 수정할 것)

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import OAuthCallback from "./pages/OAuthCallback";
 import DesignerList from "./pages/DesignerList";
 import MyReservation from "./pages/MyReservation";
 import MyReservationDetail from "./pages/MyReservationDetail";
-import PrivateRoute from "./components/PrivateRoute";
+// import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -18,21 +18,20 @@ function App() {
         {/* 기본 랜딩 페이지 (/) */}
         <Route path="/" element={<Landing />} />
         <Route path="/oauth2" element={<OAuthCallback />} />
-        {/* <Route path="/designerlist" element={<DesignerList />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/designerlist" element={<DesignerList />} />
         <Route path="/reservation" element={<Reservation />} />
         <Route path="/reservationcomplete" element={<ReservationComplete />} />
         <Route path="/designerdetail" element={<DesignerDetail />} />
         <Route path="/myreservation" element={<MyReservation />} />
-        <Route path="/myreservation/:myreservationId" element={<MyReservationDetail />} /> */}
-        <Route element={<PrivateRoute />}>
+        <Route path="/myreservation/:myreservationId" element={<MyReservationDetail />} />
+        {/* <Route element={<PrivateRoute />}>
           <Route path="/designerlist" element={<DesignerList />} />
           <Route path="/reservation" element={<Reservation />} />
           <Route path="/reservationcomplete" element={<ReservationComplete />} />
           <Route path="/designerdetail" element={<DesignerDetail />} />
           <Route path="/myreservation" element={<MyReservation />} />
           <Route path="/myreservation/:myreservationId" element={<MyReservationDetail />} />
-        </Route>
+        </Route> */}
       </Routes>
     </Router>
   );
