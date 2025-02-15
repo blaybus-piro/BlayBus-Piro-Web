8c6ed1418991fec579697fd7bdd86a7d63bd23b6
fr0gydev <ljh130334@naver.com>
2025-02-15 15:38:53 +0900

    fix :: PrivateRoute.tsx 파일 수정

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,7 +1,17 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function PrivateRoute() {
-  const accessToken = localStorage.getItem("accessToken");
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem("accessToken"));
 
-  return accessToken ? <Outlet /> : <Navigate to="/" replace />;
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setIsAuthenticated(!!localStorage.getItem("accessToken"));
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, []);
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
 }
