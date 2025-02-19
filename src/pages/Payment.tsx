abfa4355b43cf1380ab8410c531df2909cc47b91
wldy4627 <wldy4627@gmail.com>
2025-02-20 04:03:25 +0900

    startTime 난수 추가

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -44,7 +44,7 @@ export default function PaymentPage() {
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
-    const milliseconds = String(date.getMilliseconds()).padStart(6, '0'); // 마이크로초까지 확장
+    const milliseconds = String(date.getMilliseconds()).padStart(6, '1'); // 마이크로초까지 확장
 
     return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
   }
