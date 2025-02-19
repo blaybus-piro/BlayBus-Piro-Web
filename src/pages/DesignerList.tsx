997e3880521dba71ccf54554cc33ef81a5b3944d
wldy4627 <wldy4627@gmail.com>
2025-02-19 21:23:39 +0900

    feat :: 컨설팅 완료 후 상태 변화 API 연결

--- src/pages/DesignerList.tsx
+++ src/pages/DesignerList.tsx
@@ -104,9 +104,25 @@ export default function DesignerList() {
   useEffect(() => {
     if (!userId) return;
 
-    apiRequest(`/api/consulting/user/${userId}`)
-      .then((reservations) => {
+    const fetchReservations = async () => {
+      try {
+        const reservations = await apiRequest(`/api/consulting/user/${userId}`);
+
         const now = new Date();
+
+        // 예약 목록 필터링해 업데이트할 예약 찾기
+        const completedReservations = reservations.filter((reservation: ReservationState) => {
+          const reservationTime = new Date(reservation.time);
+          return reservationTime < now && reservation.status == "SCHEDULED";
+        })
+
+        // 지난 예약 상태를 COMPLETED로 업데이트
+        for (const reservation of completedReservations) {
+          await apiRequest(`/api/consulting/${reservation.id}/complete`, { method: "PATCH" });
+          reservation.status = "COMPLETE";
+        }
+
+        // 임박한 예약이 있는지 확인
         const hasUpComing = reservations.some((reservation: ReservationState) => {
           const reservationTime = new Date(reservation.time);
           const diffHours = (reservationTime.getTime() - now.getTime()) / (1000 * 60 * 60);
@@ -114,16 +130,17 @@ export default function DesignerList() {
         });
 
         setHasUpComingReservation(hasUpComing);
+
+        // ✅ 툴팁을 3초 동안 표시
         if (hasUpComing) {
           setShowTooltip(true);
-          const timeout = setTimeout(() => setShowTooltip(false), 3000);
-
-          return () => clearTimeout(timeout);
+          setTimeout(() => setShowTooltip(false), 3000);
         }
-      })
-      .catch((error) => {
-        console.error("예약 정보를 불러오는데 실패했습니다.", error);
-      });
+      } catch (error) {
+        console.error('예약 정보를 불러오는 데 실패했습니다.', error);
+      }
+    };
+    fetchReservations();
   }, [userId]);
 
   // 현재 위치 구하기
